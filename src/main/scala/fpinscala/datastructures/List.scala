10848d659249d7abec7e072036bb04fec23a0cc1
Rodney Gitzel <rodney.gitzel@dominionmarinemedia.com>
2015-09-20 19:00:21 -0700

    first work

---
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -0,0 +1,116 @@
+package fpinscala.datastructures
+
+import scala.annotation.tailrec
+
+sealed trait List[+A] // `List` data type, parameterized on a type, `A`
+case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
+/* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
+which may be `Nil` or another `Cons`.
+ */
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List { // `List` companion object. Contains functions for creating and working with lists.
+def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
+    case Nil => 0 // The sum of the empty list is 0.
+    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x,xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] = // Variadic function syntax
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  val x = List(1,2,3,4,5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+  }
+
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h,t) => Cons(h, append(t, a2))
+    }
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]) =
+    foldRight(ns, 0)((x,y) => x + y)
+
+  def product2(ns: List[Double]) =
+    foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
+
+
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(head, tail) => tail
+    }
+
+  def setHead[A](l: List[A], h: A): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(oldHead, tail) => Cons(h, tail)
+    }
+
+  def drop[A](l: List[A], n: Int): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(head, tail) =>
+        if(n > 1)
+          drop(tail, n-1)
+        else
+          tail
+    }
+
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(head, tail) =>
+        if(f(head))
+          dropWhile(tail, f)
+        else
+          l
+    }
+
+
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil              => Nil
+      case Cons(head, Nil)  => List(head)
+      case Cons(head, tail) => init(tail)
+    }
+
+
+  def length[A](l: List[A]): Int = foldRight(l, 0)((a, b) => b + 1)
+
+  @tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B =
+    l match {
+      case Nil              => z
+      case Cons(head, tail) => foldLeft(tail, f(z, head))(f)
+    }
+
+  def sumLeft(ns: List[Int]) =
+    foldLeft(ns, 0)((x,y) => x + y)
+
+  def productLeft(ns: List[Double]) =
+    foldLeft(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
+
+  def lengthLeft[A](l: List[A]): Int = foldLeft(l, 0)((b, a) => b + 1)
+
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+}
---
+++ src/test/scala/fpinscala/datastructures/ListSpec.scala
@@ -0,0 +1,170 @@
+package fpinscala.datastructures
+
+import org.scalatest._
+
+class ListSpec extends FlatSpec with Matchers {
+
+  behavior of "x"
+
+  it should "3" in {
+    List.x should be (3)
+  }
+
+
+  behavior of "tail"
+
+  it should "be Nil for Nil" in {
+    List.tail(Nil) should be (Nil)
+  }
+
+  it should "be Nil for single-item list" in {
+    List.tail(List(1)) should be (Nil)
+  }
+
+  it should "be the tail for list with a tail" in {
+    List.tail(List(1, 2)) should be (List(2))
+  }
+
+
+
+  behavior of "setHead"
+
+  it should "be Nil for Nil" in {
+    List.setHead(Nil, 1) should be (Nil)
+  }
+
+
+  it should "work for single-item list" in {
+    List.setHead(List(1), 2) should be (List(2))
+  }
+
+  it should "work for list with a tail" in {
+    List.setHead(List(1, 2), 3) should be (List(3, 2))
+  }
+
+
+
+
+  behavior of "drop"
+
+  it should "be Nil for Nil" in {
+    List.drop(Nil, 1) should be (Nil)
+  }
+
+
+  it should "work for single-item list" in {
+    List.drop(List(1), 1) should be (Nil)
+  }
+
+  it should "work for list with a tail" in {
+    List.drop(List(1, 2), 1) should be (List(2))
+  }
+
+  it should "remove the whole list is N > size" in {
+    List.drop(List(1, 2), 3) should be (Nil)
+  }
+
+
+  behavior of "dropWhile"
+
+  it should "be Nil for Nil" in {
+    List.dropWhile[Int](Nil, _ > 5) should be (Nil)
+  }
+
+
+  it should "filter single-item list" in {
+    List.dropWhile[Int](List(1), _ < 5) should be (Nil)
+  }
+
+  it should "not filter single-item list" in {
+    List.dropWhile[Int](List(7), _ < 5) should be (List(7))
+  }
+
+  it should "stop removing after a couple" in {
+    List.dropWhile[Int](List(1, 2, 3, 4, 5), _ < 3) should be (List(3, 4, 5))
+  }
+
+
+
+  behavior of "init"
+
+  it should "be Nil for Nil" in {
+    List.init(Nil) should be (Nil)
+  }
+
+  it should "be last item for single-item list" in {
+    List.init(List(1)) should be (List(1))
+  }
+
+  it should "return last item on list > 2" in {
+    List.init(List(1, 2, 3, 4, 5)) should be (List(5))
+  }
+
+
+  behavior of "foldRight and constructors"
+
+  it should "return itself" in {
+    List.foldRight(List(1,2,3), Nil:List[Int])(Cons(_, _)) should be (List(1, 2, 3))
+  }
+
+
+  behavior of "length"
+
+  it should "return 0 on empty list" in {
+    List.length(Nil) should be (0)
+  }
+
+  it should "return 1 for a single-item list" in {
+    List.length(List(1)) should be (1)
+  }
+
+  it should "return 3 for a three-item list" in {
+    List.length(List(1, 2, 3)) should be (3)
+  }
+
+
+  behavior of "sumLeft"
+
+  it should "return 0 for empty list" in {
+    List.sumLeft(Nil) should be (0)
+  }
+
+  it should "properly sum a single-item list" in {
+    List.sumLeft(List(7)) should be (7)
+  }
+
+  it should "properly sum a three-item list" in {
+    List.sumLeft(List(2,3,4)) should be (9)
+  }
+
+
+
+  behavior of "productLeft"
+
+  it should "return 1.0 for empty list" in {
+    List.productLeft(Nil) should be (1.0)
+  }
+
+  it should "properly product a single-item list" in {
+    List.productLeft(List(7)) should be (7.0)
+  }
+
+  it should "properly calculate a three-item list" in {
+    List.productLeft(List(2,3,4)) should be (24.0)
+  }
+
+
+  behavior of "lengthLeft"
+
+  it should "return 0 for empty list" in {
+    List.lengthLeft(Nil) should be (0)
+  }
+
+  it should "return 1 a single-item list" in {
+    List.lengthLeft(List(7)) should be (1)
+  }
+
+  it should "return 3 for a three-item list" in {
+    List.lengthLeft(List(2,3,4)) should be (3)
+  }
+}
