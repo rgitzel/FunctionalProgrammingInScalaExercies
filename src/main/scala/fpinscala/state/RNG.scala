13ce359c49e853b04757fe823be755cb1b40e381
Rodney Gitzel <rodney.gitzel@dominionmarinemedia.com>
2015-11-02 06:22:07 -0800

    up to exercise 6.7

---
+++ src/main/scala/fpinscala/state/RNG.scala
@@ -0,0 +1,132 @@
+package fpinscala.state
+
+import fpinscala.laziness._
+
+
+trait RNG {
+  def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
+}
+
+object RNG {
+
+  // NB - this was called SimpleRNG in the book text
+
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
+      val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
+      val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
+      (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+    }
+  }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val next = rng.nextInt
+    val nextInt = if (next._1 < 0) -(next._1 + 1) else next._1
+    (nextInt, next._2)
+  }
+
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+
+  def double(rng: RNG): (Double, RNG) = {
+    val next = nonNegativeInt(rng)
+    (next._1.toDouble / (Int.MaxValue.toDouble + 1), next._2)
+  }
+
+  def doubleViaMap: Rand[Double] = {
+    map(nonNegativeInt)(a => a.toDouble / (Int.MaxValue.toDouble + 1))
+  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val intNext = rng.nextInt
+    val doubleNext = double(intNext._2)
+    ((intNext._1, doubleNext._1), doubleNext._2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val doubleNext = double(rng)
+    val intNext = doubleNext._2.nextInt
+    ((doubleNext._1, intNext._1), intNext._2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val one = double(rng)
+    val two = double(one._2)
+    val three = double(two._2)
+    ((one._1, two._1, three._1), three._2)
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) =
+    if (count == 0)
+      (List(), rng)
+    else {
+      val head = rng.nextInt
+      val tail = ints(count - 1)(head._2)
+      (head._1 +: tail._1, tail._2)
+    }
+
+  //  def rngStream(rng: RNG): Stream[Int] = {
+  //    val next = rng.nextInt
+  //    Stream.cons(next._1, rngStream(next._2))
+  //  }
+  //
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rngA) = ra(rng)
+      val (b, rngB) = rb(rngA)
+      (f(a, b), rngB)
+    }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs match {
+      case Nil =>
+        rng => {
+          (List(), rng)
+        }
+      case head :: tail =>
+        rng => {
+          val (next, nextRng) = head(rng)
+          val rest = sequence(tail)(nextRng)
+          (next +: rest._1, rest._2)
+        }
+    }
+
+  def intsViaSequence(count: Int)(rng: RNG): (List[Int], RNG) =
+    sequence(List.fill(count)(int))(rng)
+
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+}
+
+case class State[S,+A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] =
+    sys.error("todo")
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    sys.error("todo")
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    sys.error("todo")
+}
+
+sealed trait Input
+case object Coin extends Input
+case object Turn extends Input
+
+case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+object State {
+  type Rand[A] = State[RNG, A]
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+}
---
+++ src/test/scala/fpinscala/state/RngSpec.scala
@@ -0,0 +1,174 @@
+package fpinscala.state
+
+import org.scalatest._
+
+class RngSpec extends FlatSpec with Matchers {
+
+
+	// don't call this any more times than you provide values for...
+	case class FixedRNG(nexts: List[Int]) extends RNG {
+		def nextInt = (nexts.head, FixedRNG(nexts.tail))
+	}
+
+	behavior of "nextInt"
+
+	it should "should return same random for same seed" in {
+		RNG.Simple(1847).nextInt._1 should be (RNG.Simple(1847).nextInt._1)
+	}
+
+	// 6.1
+
+	behavior of "nonNegativeValue"
+
+	def nonNegTest(in: Int, out: Int): Unit = {
+		RNG.nonNegativeInt(FixedRNG(List(in)))._1 should be (out)
+	}
+	
+	it should "can return 0" in {
+		nonNegTest(-1, 0)
+	}
+
+	it should "can return 1" in {
+		nonNegTest(1, 1)
+	}
+
+	it should "-1 should return positive" in {
+		nonNegTest(-1, 0)
+	}
+
+	it should "big positive value should return positive value" in {
+		nonNegTest(23452346, 23452346)
+	}
+
+	it should "big negative value should return positive value" in {
+		nonNegTest(-23452346, 23452345)
+	}
+
+	// it's not necessarily an even distribution, but we've shown we can generate the desired range
+
+	it should "minValue should return maxVAlue" in {
+		nonNegTest(Int.MinValue, Int.MaxValue)
+	}
+
+
+	// 6.2
+
+	behavior of "double"
+
+	it should "can return 0" in {
+		val d = RNG.double(FixedRNG(List(0, 1)))
+		d._1 should be (0.0)
+		d._2.nextInt._1 should be (1)
+	}
+
+	it should "return close to 1.0 but not 1.0" in {
+		val d = RNG.double(FixedRNG(List(Int.MaxValue)))._1
+		d should be < 1.0
+		(1.0 - d) should be < 0.000000001
+	}
+
+
+	// 6.4
+
+	behavior of "ints"
+
+	val intsRng = FixedRNG(List(45, 35, 6, 1, 55))
+	
+	it should "return empty list for 0" in {
+		val i = RNG.ints(0)(intsRng)
+		i._1 should be (Nil)
+		i._2.nextInt._1 should be (45)
+	}
+
+	it should "return 1-item list for 1" in {
+		val i = RNG.ints(1)(intsRng)
+		i._1 should be (List(45))
+		i._2.nextInt._1 should be (35)
+	}
+
+	it should "return multiple ints" in {
+		val i = RNG.ints(4)(intsRng)
+		i._1 should be (List(45, 35, 6, 1))
+		i._2.nextInt._1 should be (55)
+	}
+
+
+	// 6.5
+
+	behavior of "doubleViaMap"
+
+	it should "can return 0" in {
+		val d = RNG.doubleViaMap(FixedRNG(List(0, 1)))
+		d._1 should be (0.0)
+		d._2.nextInt._1 should be (1)
+	}
+
+	it should "return close to 1.0 but not 1.0" in {
+		val d = RNG.doubleViaMap(FixedRNG(List(Int.MaxValue)))._1
+		d should be < 1.0
+		(1.0 - d) should be < 0.000000001
+	}
+
+
+	// 6.6
+
+	behavior of "map2"
+
+	it should "can combine two functions" in {
+		val f = RNG.map2(RNG.nonNegativeInt, RNG.nonNegativeInt)((_, _))
+		val result = f(FixedRNG(List(17, 5, 44)))
+		result._1 should be ((17, 5))
+		result._2.nextInt._1 should be (44)
+	}
+
+
+	// 6.7
+
+	behavior of "sequence"
+
+	it should "can combine 0 functions" in {
+		val f = RNG.sequence(Nil)
+		val result = f(FixedRNG(List(17, 5, 44)))
+		result._1 should be (Nil)
+		result._2.nextInt._1 should be (17)
+	}
+
+	it should "can combine 1 function" in {
+		val f = RNG.sequence(List(RNG.nonNegativeInt _))
+		val result = f(FixedRNG(List(17, 5, 44)))
+		result._1 should be (List(17))
+		result._2.nextInt._1 should be (5)
+	}
+
+
+	it should "can combine 3 functions" in {
+		// 'nonNegativeInt' isn't declared as a Rand[A], but as a def, so it's not as accessible as
+		//  a "function" declared explicitly?
+		val f = RNG.sequence(List(RNG.nonNegativeEven, RNG.nonNegativeInt _, RNG.nonNegativeEven))
+		val result = f(FixedRNG(List(17, 5, 43, 12)))
+		result._1 should be (List(16, 5, 42))
+		result._2.nextInt._1 should be (12)
+	}
+
+
+	behavior of "intsViaSequence"
+
+	it should "return empty list for 0" in {
+		val i = RNG.intsViaSequence(0)(intsRng)
+		i._1 should be (Nil)
+		i._2.nextInt._1 should be (45)
+	}
+
+	it should "return 1-item list for 1" in {
+		val i = RNG.intsViaSequence(1)(intsRng)
+		i._1 should be (List(45))
+		i._2.nextInt._1 should be (35)
+	}
+
+	it should "return multiple ints" in {
+		val i = RNG.intsViaSequence(4)(intsRng)
+		i._1 should be (List(45, 35, 6, 1))
+		i._2.nextInt._1 should be (55)
+	}
+
+}
