8a5bd89eec9580f2cdbece92144209f787a6915b
Rodney Gitzel <rodney.gitzel@dominionmarinemedia.com>
2016-02-08 15:28:25 -0800

    get word count working

--- src/test/scala/fpinscala/monoids/MonoidSpec.scala
+++ src/test/scala/fpinscala/monoids/MonoidSpec.scala
@@ -175,7 +175,18 @@ class MonoidSpec extends FlatSpec with Matchers {
 
   it should "work normally with non-zero start" in {
     // to prove that the starting value doesn't get repeated throughout
-    foldRight(listToBeFolded)("= "){(a, acc) => a + acc} should be ("= " + foldedList)
+    foldRight(listToBeFolded)("x"){(a, acc) => a + acc} should be (foldedList + "x")
+  }
+
+
+  behavior of "foldLeft using foldMap"
+
+  it should "work normally with zero to start" in {
+    foldLeft(listToBeFolded)(""){(a, acc) => a + acc} should be (foldedList.reverse)
+  }
+
+  it should "work normally with non-zero start" in {
+    foldLeft(listToBeFolded)("x"){(a, acc) => a + acc} should be ("x" + foldedList.reverse)
   }
 
 
@@ -203,6 +214,50 @@ class MonoidSpec extends FlatSpec with Matchers {
 
   // 10.9
 
+
+  behavior of "orderedMonoid"
+
+  {
+    val m = orderedMonoid
+
+    it should "return first arg if second is zero" in {
+      m.op(Some(1,2), m.zero) should be (Some(1,2))
+    }
+
+    it should "return second arg if first is zero" in {
+      m.op(m.zero, Some(1,4)) should be (Some(1,4))
+    }
+
+    it should "return zero if both zero" in {
+      m.op(m.zero, m.zero) should be (m.zero)
+    }
+
+    it should "return None if second is None" in {
+      m.op(Some(1,2), None) should be (None)
+    }
+
+    it should "return None if first is None" in {
+      m.op(None, Some(1,4)) should be (None)
+    }
+
+    it should "return None if both are None" in {
+      m.op(None, None) should be (None)
+    }
+
+    it should "return Some if both are in order" in {
+      m.op(Some(1,2), Some(3,4)) should be (Some(1,4))
+    }
+
+    it should "return Some if both are in order and share the boundary" in {
+      m.op(Some(1,3), Some(3,4)) should be (Some(1,4))
+    }
+
+    it should "return None if they are not in order" in {
+      m.op(Some(1,3), Some(2,4)) should be (None)
+    }
+  }
+
+
   behavior of "ordered"
 
   it should "return true for empty seq" in {
@@ -221,9 +276,9 @@ class MonoidSpec extends FlatSpec with Matchers {
     ordered(Vector(3, 2)) should be (false)
   }
 
-  val bigSeq = (1.to(100)).toIndexedSeq
+  val bigSeq = (-100.to(100)).toIndexedSeq
 
-  it should "return true for big sequence" in {
+  it should "return true for big sequence both negative and positive" in {
     ordered(bigSeq) should be (true)
   }
 
@@ -235,8 +290,90 @@ class MonoidSpec extends FlatSpec with Matchers {
     val bad = bigSeq ++ Vector(0)
     ordered(bad) should be (false)
   }
+
+  it should "work for the extremes" in {
+    ordered(Vector(Int.MinValue, 0, Int.MaxValue)) should be (true)
+  }
+
+
+  // 10.10
+
+  behavior of "wcMonoid"
+
+  {
+    val m = wcMonoid
+
+    it should "return first arg if second is zero" in {
+      m.op(Stub("a"), m.zero) should be(Stub("a"))
+    }
+
+    it should "return second arg if first is zero" in {
+      m.op(m.zero, Stub("a")) should be(Stub("a"))
+    }
+
+    it should "return zero if both zero" in {
+      m.op(m.zero, m.zero) should be(m.zero)
+    }
+
+    it should "combine two stubs" in {
+      m.op(Stub("x"), Stub("y")) should be (Stub("xy"))
+    }
+
+    it should "combine a stub on the left with a part with a left bit" in {
+      m.op(Stub("x"), Part("b", 0, "c")) should be(Part("xb", 0, "c"))
+    }
+
+    it should "combine a stub on the left with a part without a left bit" in {
+      m.op(Stub("x"), Part("", 0, "c")) should be(Part("x", 0, "c"))
+    }
+
+    it should "combine a stub on the right with a part with a right bit" in {
+      m.op(Part("b", 0, "c"), Stub("x")) should be(Part("b", 0, "cx"))
+    }
+
+    it should "combine a stub on the right with a part without a left bit" in {
+      m.op(Part("b", 0, ""), Stub("x")) should be(Part("b", 0, "x"))
+    }
+
+    it should "combine two parts with a shared word" in {
+      m.op(Part("", 0, "a"), Part("b", 0, "")) should be(Part("", 1, ""))
+    }
+
+    it should "combine two parts without a shared word" in {
+      m.op(Part("", 3, ""), Part("", 1, "")) should be(Part("", 4, ""))
+    }
+  }
+
+  // 10.11
+
+  behavior of "count"
+
+  it should "return 0 for empty string" in {
+    count("") should be (0)
+  }
+
+  it should "return 1 for single character" in {
+    count("x") should be (1)
+  }
+
+  it should "return 1 for multiple characters without space" in {
+    count("xyz") should be (1)
+  }
+
+  it should "return 2 for two words" in {
+    count(" x z ") should be (2)
+  }
+
+  it should "return correct count for first book example" in {
+    count("lorem ipsum do") should be (3)
+  }
+
+  it should "return correct count for second book example" in {
+    count("lor sit amet, ") should be (3)
+  }
+
+  it should "return correct count with preceeding space" in {
+    count(" lor sit amet, ") should be (3)
+  }
 }
-
-
-
 
