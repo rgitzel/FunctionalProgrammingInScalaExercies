29ef4c3505ed6c7c8adf005f5a024b83268024aa
Rodney Gitzel <rodney.gitzel@dominionmarinemedia.com>
2016-03-03 21:39:50 -0800

    12.2

---
+++ src/main/scala/fpinscala/applicative/Applicative.scala
@@ -0,0 +1,207 @@
+package fpinscala.applicative
+
+import fpinscala.monads.Functor
+import fpinscala.monoids.Monoid
+
+import scala.language.{higherKinds, implicitConversions}
+
+trait Applicative[F[_]] extends Functor[F] {
+
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+
+  def map3[A,B,C,D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] = {
+    val fab = map2(fa, fb){ case(a, b) =>
+      f.curried(a)(b)
+    }
+    map2(fab, fc){ case (ab, c) =>
+      ab(c)
+    }
+  }
+
+  def map4[A,B,C,D,E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => E): F[E] = {
+    val fabc = map3(fa, fb, fc){ case(a, b, c) =>
+      f.curried(a)(b)(c)
+    }
+    map2(fabc, fd){ case (abc, d) =>
+      abc(d)
+    }
+  }
+
+  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = ???
+
+  def unit[A](a: => A): F[A]
+
+  def map[A,B](fa: F[A])(f: A => B): F[B] =
+    apply(unit(f))(fa)
+
+  def sequence[A](fas: List[F[A]]): F[List[A]] =
+    fas.foldLeft(unit(List[A]())){ (acc, a) =>
+      map2(acc, a)(_ :+ _)
+    }
+
+  def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] = ???
+
+  def product[A,B](fa: F[A], fb: F[B]): F[(A,B)] =
+    map2(fa, fb)( (_, _) )
+
+  def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
+    if(n == 0)
+      unit(Nil)
+    else
+      map2(fa, replicateM(n - 1, fa))(_ +: _)
+
+  def factor[A,B](fa: F[A], fb: F[B]): F[(A,B)] = ???
+
+  //def product[G[_]](G: Applicative[G]): Applicative[({type f[x] = (F[x], G[x])})#f] = ???
+
+  def compose[G[_]](G: Applicative[G]): Applicative[({type f[x] = F[G[x]]})#f] = ???
+
+  def sequenceMap[K,V](ofa: Map[K,F[V]]): F[Map[K,V]] = ???
+}
+
+object Applicative {
+  val listApplicative = new Applicative[List] {
+    def unit[A](a: => A) = List(a)
+    def map2[A,B,C](fa: List[A], fb: List[B])(f: (A, B) => C) =
+      for {
+        a <- fa
+        b <- fb
+      } yield f(a, b)
+  }
+
+  val optionApplicative = new Applicative[Option] {
+    def unit[A](a: => A) = Some(a)
+    def map2[A,B,C](fa: Option[A], fb: Option[B])(f: (A, B) => C) =
+      for {
+        a <- fa
+        b <- fb
+      } yield f(a, b)
+  }
+
+}
+
+
+//case class Tree[+A](head: A, tail: List[Tree[A]])
+//
+//trait Monad[F[_]] extends Applicative[F] {
+//  def flatMap[A,B](ma: F[A])(f: A => F[B]): F[B] = join(map(ma)(f))
+//
+//  def join[A](mma: F[F[A]]): F[A] = flatMap(mma)(ma => ma)
+//
+//  def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C] =
+//    a => flatMap(f(a))(g)
+//
+//  override def apply[A,B](mf: F[A => B])(ma: F[A]): F[B] =
+//    flatMap(mf)(f => map(ma)(a => f(a)))
+//}
+//
+//object Monad {
+//  def eitherMonad[E]: Monad[({type f[x] = Either[E, x]})#f] = ???
+//
+//  def stateMonad[S] = new Monad[({type f[x] = State[S, x]})#f] {
+//    def unit[A](a: => A): State[S, A] = State(s => (a, s))
+//    override def flatMap[A,B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
+//      st flatMap f
+//  }
+//
+//  def composeM[F[_],N[_]](implicit F: Monad[F], N: Monad[N], T: Traverse[N]):
+//  Monad[({type f[x] = F[N[x]]})#f] = ???
+//}
+//
+//sealed trait Validation[+E, +A]
+//
+//case class Failure[E](head: E, tail: Vector[E])
+//  extends Validation[E, Nothing]
+//
+//case class Success[A](a: A) extends Validation[Nothing, A]
+//
+//
+//object Applicative {
+//
+//  val streamApplicative = new Applicative[Stream] {
+//
+//    def unit[A](a: => A): Stream[A] =
+//      Stream.continually(a) // The infinite, constant stream
+//
+//    override def map2[A,B,C](a: Stream[A], b: Stream[B])( // Combine elements pointwise
+//                                                          f: (A,B) => C): Stream[C] =
+//      a zip b map f.tupled
+//  }
+//
+//  def validationApplicative[E]: Applicative[({type f[x] = Validation[E,x]})#f] = ???
+//
+//  type Const[A, B] = A
+//
+//  implicit def monoidApplicative[M](M: Monoid[M]) =
+//    new Applicative[({ type f[x] = Const[M, x] })#f] {
+//      def unit[A](a: => A): M = M.zero
+//      override def apply[A,B](m1: M)(m2: M): M = M.op(m1, m2)
+//    }
+//}
+//
+//trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
+//  def traverse[G[_]:Applicative,A,B](fa: F[A])(f: A => G[B]): G[F[B]] =
+//    sequence(map(fa)(f))
+//  def sequence[G[_]:Applicative,A](fma: F[G[A]]): G[F[A]] =
+//    traverse(fma)(ma => ma)
+//
+//  type Id[A] = A
+//  val idMonad = new Monad[Id] {
+//    def unit[A](a: => A) = a
+//    override def flatMap[A,B](a: A)(f: A => B): B = f(a)
+//  }
+//
+//  def map[A,B](fa: F[A])(f: A => B): F[B] =
+//    traverse[Id, A, B](fa)(f)(idMonad)
+//
+//  import Applicative._
+//
+//  override def foldMap[A,B](as: F[A])(f: A => B)(mb: Monoid[B]): B =
+//    traverse[({type f[x] = Const[B,x]})#f,A,Nothing](
+//      as)(f)(monoidApplicative(mb))
+//
+//  def traverseS[S,A,B](fa: F[A])(f: A => State[S, B]): State[S, F[B]] =
+//    traverse[({type f[x] = State[S,x]})#f,A,B](fa)(f)(Monad.stateMonad)
+//
+//  def mapAccum[S,A,B](fa: F[A], s: S)(f: (A, S) => (B, S)): (F[B], S) =
+//    traverseS(fa)((a: A) => (for {
+//      s1 <- get[S]
+//      (b, s2) = f(a, s1)
+//      _  <- set(s2)
+//    } yield b)).run(s)
+//
+//  override def toList[A](fa: F[A]): List[A] =
+//    mapAccum(fa, List[A]())((a, s) => ((), a :: s))._2.reverse
+//
+//  def zipWithIndex[A](fa: F[A]): F[(A, Int)] =
+//    mapAccum(fa, 0)((a, s) => ((a, s), s + 1))._1
+//
+//  def reverse[A](fa: F[A]): F[A] = ???
+//
+//  override def foldLeft[A,B](fa: F[A])(z: B)(f: (B, A) => B): B = ???
+//
+//  def fuse[G[_],H[_],A,B](fa: F[A])(f: A => G[B], g: A => H[B])
+//                         (implicit G: Applicative[G], H: Applicative[H]): (G[F[B]], H[F[B]]) = ???
+//
+//  def compose[G[_]](implicit G: Traverse[G]): Traverse[({type f[x] = F[G[x]]})#f] = ???
+//}
+//
+//object Traverse {
+//  val listTraverse = ???
+//
+//  val optionTraverse = ???
+//
+//  val treeTraverse = ???
+//}
+//
+//// The `get` and `set` functions on `State` are used above,
+//// but aren't in the `exercises` subproject, so we include
+//// them here
+//object StateUtil {
+//
+//  def get[S]: State[S, S] =
+//    State(s => (s, s))
+//
+//  def set[S](s: S): State[S, Unit] =
+//    State(_ => ((), s))
+//}
---
+++ src/test/scala/fpinscala/applicative/ApplicativeSpec.scala
@@ -0,0 +1,84 @@
+package fpinscala.applicative
+
+import org.scalatest._
+import Applicative._
+
+class ApplicativeSpec extends FlatSpec with Matchers {
+
+//  behavior of "listApplicative"
+//
+//  it should "map2 correctly" in {
+//    listApplicative.map2(List(1, 2), List(2, 3))(_ + _) should be (List(3, 5))
+//  }
+
+
+  behavior of "optionApplicative"
+
+  def sum(a: Int, b: Int) = a + b
+
+  it should "map2 two Somes to a Some" in {
+    optionApplicative.map2(Some(1), Some(2))(sum) should be (Some(3))
+  }
+
+  it should "map2 Some/None to a None" in {
+    optionApplicative.map2(Some(1), None)(sum) should be (None)
+  }
+
+  it should "map2 None/Some to a None" in {
+    optionApplicative.map2(None, Some(1))(sum) should be (None)
+  }
+
+
+  // 12.1
+
+
+  behavior of "sequence for options"
+
+  it should "convert List[Option] to Option[List]" in {
+    optionApplicative.sequence(List(Some(1), Some(2), Some(0))) should be (Some(List(1, 2, 0)))
+  }
+
+  it should "convert empty list to option of empty list" in {
+    optionApplicative.sequence(Nil) should be (Some(Nil))
+  }
+
+
+  behavior of "replicatedM for options"
+
+  it should "convert an option to a list of that option" in {
+    val o = Some(1)
+    optionApplicative.replicateM(4, o) should be (Some(List(1, 1, 1, 1)))
+  }
+
+  it should "convert option to empty list" in {
+    optionApplicative.replicateM(0, Some(1)) should be (Some(Nil))
+  }
+
+
+  behavior of "product for options"
+
+  it should "combine two options" in {
+    optionApplicative.product(Some(1), Some(2)) should be (Some((1, 2)))
+  }
+
+
+  // 12.2
+
+  behavior of "map3 for options"
+
+  def sum3(a: Int, b: Int, c: Int) = a + b + c
+
+  it should "combine three somes" in {
+    optionApplicative.map3(Some(1), Some(2), Some(3))(sum3) should be (Some(6))
+  }
+
+
+  behavior of "map3 for options"
+
+  def sum4(a: Int, b: Int, c: Int, d: Int) = a + b + c + d
+
+  it should "combine four somes" in {
+    optionApplicative.map4(Some(1), Some(2), Some(3), Some(4))(sum4) should be (Some(10))
+  }
+
+}
